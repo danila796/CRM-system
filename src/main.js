0b8f19c93c9d59c44b9842f5d828ce845056436c
Danil Kiriukhin <powerdan777@gmail.com>
2020-09-17 00:15:38 +0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router/index'
+import store from './store/index'
 import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import 'materialize-css/dist/js/materialize.min';
+import dateFilter from './filters/date.filter'
 
 Vue.config.productionTip = false
+
+Vue.filter('date', dateFilter)
 
 new Vue({
   router,
