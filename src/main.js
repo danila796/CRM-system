3746c0c00906d16f2616f90ea333f5549dc03905
Danil Kiriukhin <powerdan777@gmail.com>
2020-11-24 02:03:16 +0300

    Account Page. Added Name of the account user to the Navbar. Added Loader (when page is loading). Added money conversion into EUR, US and RUB from bill (sort of purse with conversion functional). Added currency rate table for present day. Added refresh button

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import dateFilter from '@/filters/date.filter'
+import currencyFilter from '@/filters/currency.filter'
 import messagePlugin from '@/utils/message.plugin'
+import Loader from '@/components/app/Loader'
 import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min'
 
@@ -14,10 +16,13 @@ import 'firebase/database'
 
 
 Vue.config.productionTip = false
+Vue.config.devtools = true
 
 Vue.use(messagePlugin)
 Vue.use(Vuelidate)
 Vue.filter('date', dateFilter)
+Vue.filter('currency', currencyFilter)
+Vue.component('Loader', Loader)
 
 const firebaseConfig = {
   apiKey: "AIzaSyAQlODwMaNSQMubQBgt1ikrliTJwDo8Z74",
